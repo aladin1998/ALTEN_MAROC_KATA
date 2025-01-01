6da355ff6dbccf677c1fbe2e82af7f0481f9e8d4
alaeeddine.cherqi <cherki.ronaldo@gmail.com>
2025-01-01 13:50:40 +0100

    add README file for backend side

---
+++ back/back-products/src/main/java/com/example/back_products/BackProductsApplication.java
@@ -0,0 +1,14 @@
+package com.example.back_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BackProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackProductsApplication.class, args);
+	}
+
+}
